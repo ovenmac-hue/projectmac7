580b647a05f0cc91b45305520c20424008cbfc14
MacThas <ovenmac@gmail.com>
2026-02-13 10:28:56 +0700

    #

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
+#include<ctime>
+
+long long int fibonacci(long long int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(long long int x){
+    if (x == 0){
+        x = 0;
+    }else if(x == 1){
+        x = 1;
+    }else if(x > 1){
+        x = fibonacci(x-1)+fibonacci(x-2);
+    }
+    return x;
 }
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+long long int gcd(long long int x, long long int y);
+
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+long long int gcd(long long int x, long long int y){
+    if (x%y == 0){
+        return y;
+    }else{
+        return gcd(y, x%y);
+    }
 }
